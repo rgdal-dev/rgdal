e9f5b5afea24cff77cab16379753270e805bb8db
rsbivand (Roger Bivand) <roger.bivand@nhh.no>
2012-12-03 18:43:01 +0000

    rgeos not rgdal SVN
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgdal@391 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAA.R
+++ pkg/R/AAA.R
@@ -31,10 +31,9 @@ assign(".rgdal_old.GDAL_DATA", "", envir=.RGDAL_CACHE)
   gdl <- getGDAL_DATA_Path()
   pl <- getPROJ4libPath()
   if (nchar(pl) == 0) pl <- "(autodetected)"
-  fn <- system.file("SVN_VERSION", package="rgeos")
+  fn <- system.file("SVN_VERSION", package="rgdal")
   if (file.exists(fn)) {
-    svn_version <- scan(system.file("SVN_VERSION", package="rgeos"),
-      what=character(1), sep="\n", quiet=TRUE)
+    svn_version <- scan(fn, what=character(1), sep="\n", quiet=TRUE)
   } else {
     svn_version <- "(unknown)"
   }
