ef6848a63cab8966ca5c709f280ae009f14e1a80
rsbivand (Roger Bivand) <roger.bivand@nhh.no>
2015-05-21 09:02:38 +0000

    work-around for missing proj_def.dat
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgdal@529 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAA.R
+++ pkg/R/AAA.R
@@ -22,6 +22,7 @@ assign(".rgdal_old.GDAL_DATA", "", envir=.RGDAL_CACHE)
   }
   assign("OVERRIDE_PROJ_DATUM_WITH_TOWGS84", TRUE, envir=.RGDAL_CACHE)
   assign("silent", TRUE, envir=.RGDAL_CACHE)
+  assign("has_proj_defs.dat", .Call("PROJ4_proj_def_dat_Installed", PACKAGE="rgdal"), envir=.RGDAL_CACHE)
 
   library.dynam('rgdal', pkg, lib)
 
