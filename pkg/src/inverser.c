7ac9627df9168e5b59014d31eedf0ff42fbcce01
rsbivand (Roger Bivand) <roger.bivand@nhh.no>
2020-06-05 10:13:30 +0000

    checking multiple exSoft versions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgdal@999 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/inverser.c
+++ pkg/src/inverser.c
@@ -1,15 +1,17 @@
 /* Copyright (c) 2016 Barry Rowlingson */
 typedef int make_iso_compilers_happy;
 
+#ifndef NEW_PROJ_WITH_OLD_GDAL
 #ifndef PROJ_H_API
+#include <projects.h>
 #include <proj_api.h>
 #if PJ_VERSION < 493
-#include <projects.h>
 
 int inversetest(PJ *P){
   return (P->inv ? 1: 0);
 
 }
+#endif
 #endif
 #endif
 
